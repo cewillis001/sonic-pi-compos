3854fc88aecebe039aa70d25894fe93d2ff6edb0
Caroline <cewillis001@users.noreply.github.com>
2019-10-09 19:05:34 -0700

    Adding bar struct and transpose function
    YEAH WE DID :D :D :D

--- men of erin/men_of_erin.rb
+++ men of erin/men_of_erin.rb
@@ -16,6 +16,11 @@ play_alto = true
 play_tenor = true
 play_bass = true
 
+BarStruct = Struct.new(:pitches, :durations, :dynamic_start, :dynamic_end) do
+  def to_ary
+    [pitches, durations, dynamic_start, dynamic_end]
+  end
+end
 
 def play_bar (pitches, durations, dynamic_start, dynamic_end)
   volumes = (line dynamic_start, dynamic_end, steps: (pitches.length), inclusive: true)
@@ -24,59 +29,66 @@ def play_bar (pitches, durations, dynamic_start, dynamic_end)
   end
 end
 
+# musical_line is array of BarStructs
+# interval is a positive or negative number that will be used to shift pitches
+# +12 will shift up an octave
+# -2 shifts down one whole step
+# octave is five whole steps + 2 half steps, because music theory?
+
+def transpose (musical_line, interval)
+  new_musical_line = []
+  for bar in musical_line
+    new_bar = BarStruct.new(bar.pitches, bar.durations, bar.dynamic_start, bar.dynamic_end)
+    new_bar.pitches = new_bar.pitches.map { |note| note + interval}
+    new_musical_line << new_bar
+  end
+  new_musical_line
+end
+
+
 # 16 bars
 # 1 beat = 1 quarter note
-soprano_line = [
-  [[:r,:d4,:e4],[2,0.5,0.5],mezzo_forte,mezzo_forte],
+melody_line = [
+  BarStruct.new([:r,:d4,:e4],[2,0.5,0.5],mezzo_forte,mezzo_forte),
   
-  [[:g4,:a4],[2.75,0.25],mezzo_forte,mezzo_forte],
-  [[:b4,:a4,:g4],[2,0.25,0.75],mezzo_forte,mezzo_forte],
-  [[:g4,:g4,:e4],[0.5,2,0.5],mezzo_forte,mezzo_forte],
-  [[:d4,:d4,:e4],[2,0.5,0.5],mezzo_forte,mezzo_forte],
+  BarStruct.new([:g4,:a4],[2.75,0.25],mezzo_forte,mezzo_forte),
+  BarStruct.new([:b4,:a4,:g4],[2,0.25,0.75],mezzo_forte,mezzo_forte),
+  BarStruct.new([:g4,:g4,:e4],[0.5,2,0.5],mezzo_forte,mezzo_forte),
+  BarStruct.new([:d4,:d4,:e4],[2,0.5,0.5],mezzo_forte,mezzo_forte),
   
-  [[:g4,:g4,:g4],[2,0.75,0.25],mezzo_forte,mezzo_forte],
-  [[:b4,:d5,:b4],[0.5,2,0.5],mezzo_forte,mezzo_forte],
-  [[:a4,:a4,:b4],[0.5,2,0.5],mezzo_forte,mezzo_forte],
-  [[:a4,:b4,:d5],[2,0.25,0.75],mezzo_forte,mezzo_forte],
+  BarStruct.new([:g4,:g4,:g4],[2,0.75,0.25],mezzo_forte,mezzo_forte),
+  BarStruct.new([:b4,:d5,:b4],[0.5,2,0.5],mezzo_forte,mezzo_forte),
+  BarStruct.new([:a4,:a4,:b4],[0.5,2,0.5],mezzo_forte,mezzo_forte),
+  BarStruct.new([:a4,:b4,:d5],[2,0.25,0.75],mezzo_forte,mezzo_forte),
   
-  [[:e5,:d5,:d5,:d5],[1,1,0.5,0.5],mezzo_forte,mezzo_forte],
-  [[:e5,:d5,:b4,:b4],[0.25,1.75,0.5,0.5],mezzo_forte,mezzo_forte],
-  [[:a4,:g4,:e4],[0.5,2,0.5],mezzo_forte,mezzo_forte],
-  [[:d4,:d4,:e4],[2,0.25,0.75],mezzo_forte,mezzo_forte],
+  BarStruct.new([:e5,:d5,:d5,:d5],[1,1,0.5,0.5],mezzo_forte,mezzo_forte),
+  BarStruct.new([:e5,:d5,:b4,:b4],[0.25,1.75,0.5,0.5],mezzo_forte,mezzo_forte),
+  BarStruct.new([:a4,:g4,:e4],[0.5,2,0.5],mezzo_forte,mezzo_forte),
+  BarStruct.new([:d4,:d4,:e4],[2,0.25,0.75],mezzo_forte,mezzo_forte),
   
-  [[:g4,:e4,:d4],[2,0.25,0.75],mezzo_forte,mezzo_forte],
-  [[:b4,:g4,:g4],[1,1.5,0.5],mezzo_forte,mezzo_forte],
-  [[:g4],[3],mezzo_forte,mezzo_forte]
+  BarStruct.new([:g4,:e4,:d4],[2,0.25,0.75],mezzo_forte,mezzo_forte),
+  BarStruct.new([:b4,:g4,:g4],[1,1.5,0.5],mezzo_forte,mezzo_forte),
+  BarStruct.new([:g4],[3],mezzo_forte,mezzo_forte)
 ]
 
-tenor_line = [
-  [[:r,:d3,:e3],[2,0.5,0.5],mezzo_forte,mezzo_forte],
-  
-  [[:g3,:a3],[2.75,0.25],mezzo_forte,mezzo_forte],
-  [[:b3,:a3,:g3],[2,0.25,0.75],mezzo_forte,mezzo_forte],
-  [[:g3,:g3,:e3],[0.5,2,0.5],mezzo_forte,mezzo_forte],
-  [[:d3,:d3,:e3],[2,0.5,0.5],mezzo_forte,mezzo_forte],
-  
-  [[:g3,:g3,:g3],[2,0.75,0.25],mezzo_forte,mezzo_forte],
-  [[:b3,:d4,:b3],[0.5,2,0.5],mezzo_forte,mezzo_forte],
-  [[:a3,:a3,:b3],[0.5,2,0.5],mezzo_forte,mezzo_forte],
-  [[:a3,:b3,:d4],[2,0.25,0.75],mezzo_forte,mezzo_forte],
-  
-  [[:e4,:d4,:d4,:d4],[1,1,0.5,0.5],mezzo_forte,mezzo_forte],
-  [[:e4,:d4,:b3,:b3],[0.25,1.75,0.5,0.5],mezzo_forte,mezzo_forte],
-  [[:a3,:g3,:e3],[0.5,2,0.5],mezzo_forte,mezzo_forte],
-  [[:d3,:d3,:e3],[2,0.25,0.75],mezzo_forte,mezzo_forte],
-  
-  [[:g3,:e3,:d3],[2,0.25,0.75],mezzo_forte,mezzo_forte],
-  [[:b3,:g3,:g3],[1,1.5,0.5],mezzo_forte,mezzo_forte],
-  [[:g3],[3],mezzo_forte,mezzo_forte]
-]
+octave = 12
+semitone = 1
+major_second = 2
+major_third = 4
+perfect_forth = 5
+perfect_fifth = 7
+major_sixth = 9
+major_seventh = 11
+
+t_interval = -(major_second)
 
 if play_soprano
   in_thread do
+    soprano_line = Array.new(melody_line)
+    soprano_line = transpose(soprano_line, t_interval)
     6.times do
       for bar in soprano_line
-        play_bar(bar[0], bar[1], bar[2], bar[3])
+        play_bar(bar.pitches, bar.durations, bar.dynamic_start, bar.dynamic_end)
       end
     end
   end
@@ -90,9 +102,12 @@ end
 
 if play_tenor
   in_thread do
+    tenor_line = Array.new(melody_line)
+    tenor_line = transpose(tenor_line, -(octave))
+    tenor_line = transpose(tenor_line, t_interval)
     6.times do
       for bar in tenor_line
-        play_bar(bar[0], bar[1], bar[2], bar[3])
+        play_bar(bar.pitches, bar.durations, bar.dynamic_start, bar.dynamic_end)
       end
     end
   end
