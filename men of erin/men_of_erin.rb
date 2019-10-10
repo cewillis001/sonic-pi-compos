a381319a66e0d24a039b519dc690fc1e4a61a79d
Caroline <cewillis001@users.noreply.github.com>
2019-10-09 17:03:18 -0700

    Update men_of_erin.rb

--- men of erin/men_of_erin.rb
+++ men of erin/men_of_erin.rb
@@ -7,17 +7,15 @@ mezzo_forte = 0.4
 forte = 0.5
 fortissimo = 0.6
 
-# use_bpm 50 * 3
+use_bpm 100
 
-use_synth :piano
+use_synth :beep
 
 play_soprano = true
 play_alto = true
 play_tenor = true
 play_bass = true
 
-# pitches, durations, volumes should have same number of elements
-# maybe I could make volumes a ring?
 
 def play_bar (pitches, durations, dynamic_start, dynamic_end)
   volumes = (line dynamic_start, dynamic_end, steps: (pitches.length), inclusive: true)
@@ -37,26 +35,50 @@ soprano_line = [
   [[:d4,:d4,:e4],[2,0.5,0.5],mezzo_forte,mezzo_forte],
   
   [[:g4,:g4,:g4],[2,0.75,0.25],mezzo_forte,mezzo_forte],
-  [[],[],mezzo_forte,mezzo_forte],
-  [[],[],mezzo_forte,mezzo_forte],
-  [[],[],mezzo_forte,mezzo_forte],
+  [[:b4,:d5,:b4],[0.5,2,0.5],mezzo_forte,mezzo_forte],
+  [[:a4,:a4,:b4],[0.5,2,0.5],mezzo_forte,mezzo_forte],
+  [[:a4,:b4,:d5],[2,0.25,0.75],mezzo_forte,mezzo_forte],
   
-  [[],[],mezzo_forte,mezzo_forte],
-  [[],[],mezzo_forte,mezzo_forte],
-  [[],[],mezzo_forte,mezzo_forte],
-  [[],[],mezzo_forte,mezzo_forte],
+  [[:e5,:d5,:d5,:d5],[1,1,0.5,0.5],mezzo_forte,mezzo_forte],
+  [[:e5,:d5,:b4,:b4],[0.25,1.75,0.5,0.5],mezzo_forte,mezzo_forte],
+  [[:a4,:g4,:e4],[0.5,2,0.5],mezzo_forte,mezzo_forte],
+  [[:d4,:d4,:e4],[2,0.25,0.75],mezzo_forte,mezzo_forte],
   
-  [[],[],mezzo_forte,mezzo_forte],
-  [[],[],mezzo_forte,mezzo_forte],
-  [[],[],mezzo_forte,mezzo_forte]
+  [[:g4,:e4,:d4],[2,0.25,0.75],mezzo_forte,mezzo_forte],
+  [[:b4,:g4,:g4],[1,1.5,0.5],mezzo_forte,mezzo_forte],
+  [[:g4],[3],mezzo_forte,mezzo_forte]
+]
+
+tenor_line = [
+  [[:r,:d3,:e3],[2,0.5,0.5],mezzo_forte,mezzo_forte],
+  
+  [[:g3,:a3],[2.75,0.25],mezzo_forte,mezzo_forte],
+  [[:b3,:a3,:g3],[2,0.25,0.75],mezzo_forte,mezzo_forte],
+  [[:g3,:g3,:e3],[0.5,2,0.5],mezzo_forte,mezzo_forte],
+  [[:d3,:d3,:e3],[2,0.5,0.5],mezzo_forte,mezzo_forte],
+  
+  [[:g3,:g3,:g3],[2,0.75,0.25],mezzo_forte,mezzo_forte],
+  [[:b3,:d4,:b3],[0.5,2,0.5],mezzo_forte,mezzo_forte],
+  [[:a3,:a3,:b3],[0.5,2,0.5],mezzo_forte,mezzo_forte],
+  [[:a3,:b3,:d4],[2,0.25,0.75],mezzo_forte,mezzo_forte],
+  
+  [[:e4,:d4,:d4,:d4],[1,1,0.5,0.5],mezzo_forte,mezzo_forte],
+  [[:e4,:d4,:b3,:b3],[0.25,1.75,0.5,0.5],mezzo_forte,mezzo_forte],
+  [[:a3,:g3,:e3],[0.5,2,0.5],mezzo_forte,mezzo_forte],
+  [[:d3,:d3,:e3],[2,0.25,0.75],mezzo_forte,mezzo_forte],
+  
+  [[:g3,:e3,:d3],[2,0.25,0.75],mezzo_forte,mezzo_forte],
+  [[:b3,:g3,:g3],[1,1.5,0.5],mezzo_forte,mezzo_forte],
+  [[:g3],[3],mezzo_forte,mezzo_forte]
 ]
 
 if play_soprano
   in_thread do
-    for bar in soprano_line
-      play_bar(bar[0], bar[1], bar[2], bar[3])
+    6.times do
+      for bar in soprano_line
+        play_bar(bar[0], bar[1], bar[2], bar[3])
+      end
     end
-    
   end
 end
 
@@ -67,15 +89,11 @@ if play_alto
 end
 
 if play_tenor
-  #define bars
-  bars = Array.new(1)
-  note_list = [:c4,:d4,:e4,:c4]
-  len_list = [1,2,1,1]
-  play_bar(note_list, len_list, forte, pianissimo)
-  
   in_thread do
-    bars.each do |bar|
-      #bar.play
+    6.times do
+      for bar in tenor_line
+        play_bar(bar[0], bar[1], bar[2], bar[3])
+      end
     end
   end
 end
